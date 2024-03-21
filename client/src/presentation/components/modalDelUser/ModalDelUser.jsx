f8cc2bcd3fd72fdbb010831c11846db5a766d6e5
Abel Aron Amieva <abelamieva@gmail.com>
2024-03-20 21:21:06 -0300

    se creo el modal para eliminar el usuario

---
+++ client/src/presentation/components/modalDelUser/ModalDelUser.jsx
@@ -0,0 +1,44 @@
+import { Button, Divider, Modal, Typography } from "antd";
+import { useModalDelUser } from "../../hooks/useModalDelUser";
+
+export const ModalDelUser = ({ user }) => {
+
+    const {
+        close,
+        open,
+        isOpen,
+        onConfirm,
+        isLoading
+    } = useModalDelUser();
+
+    return (
+        <div>
+            <Button type='link' onClick={open}>
+                Eliminar
+            </Button>
+            <Modal
+                destroyOnClose
+                centered
+                title={'Eliminar usuario'}
+                open={isOpen}
+                confirmLoading={isLoading}
+                onOk={() => onConfirm(user.id)}
+                onCancel={close}
+                width={'438px'}
+                okText='Eliminar'
+                cancelText='Cancelar'
+                okType="primary"
+                okButtonProps={{
+                    danger: true
+                }}
+            >
+                <Divider />
+                <Typography.Text strong>
+                    Está seguro que quiere eliminar el usuario <Typography.Text type='danger'>@{user.username}</Typography.Text>?
+                </Typography.Text>
+                <Divider />
+            </Modal>
+
+        </div>
+    )
+}
