44a38807571ac8a49a15d5d04515530ac2e93afc
Abel Aron Amieva <abelamieva@gmail.com>
2024-03-20 22:24:50 -0300

    se realiza la obtencion de los usuarios basados en el filter

--- client/src/presentation/hooks/useFilter.jsx
+++ client/src/presentation/hooks/useFilter.jsx
@@ -1,15 +1,18 @@
 import { useDispatch } from "react-redux";
 import { setFilterByName, setFilterByStatus } from "../redux/features/users/usersSlice";
+import { getUsersThunks } from "../redux";
 
 export const useFilter = () => {
     const dispatch = useDispatch();
 
     const handleFilterByStatus = (value) => {
         dispatch(setFilterByStatus(value));
+        dispatch(getUsersThunks())
     };
 
     const handleFilterByName = (value = '') => {
         dispatch(setFilterByName(value.trim()))
+        dispatch(getUsersThunks())
     };
     return {
         handleFilterByStatus,
