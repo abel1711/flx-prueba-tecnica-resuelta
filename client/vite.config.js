b8d8dff13dc577af57f4af2d9a0160cf4e078657
Abel Aron Amieva <abelamieva@gmail.com>
2024-03-21 00:02:57 -0300

    se crearon los alias para los path

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import * as path from 'path';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  resolve: {
+    alias: [
+      { find: "@", replacement: path.resolve(__dirname, "src") },
+      { find: "@components", replacement: path.resolve(__dirname, "src/presentation/components") },
+      { find: "@hooks", replacement: path.resolve(__dirname, "src/presentation/hooks") },
+      { find: "@redux", replacement: path.resolve(__dirname, "src/presentation/redux") },
+      { find: "@router", replacement: path.resolve(__dirname, "src/presentation/router") },
+      { find: "@screens", replacement: path.resolve(__dirname, "src/presentation/screens") },
+      { find: "@config", replacement: path.resolve(__dirname, "src/config") },
+    ],
+  },
 })
