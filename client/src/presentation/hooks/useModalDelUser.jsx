a4e4ef4175149521f4c2d4847774a4aa01e3225d
Abel Aron Amieva <abelamieva@gmail.com>
2024-03-20 21:21:42 -0300

    se creo el hook para el manejo del modal para eliminar usuario

---
+++ client/src/presentation/hooks/useModalDelUser.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { userApi } from "../../config/api/user-api";
+import { getUsersThunks } from "../redux/features/users/usersThunks";
+
+export const useModalDelUser = () => {
+
+  const dispatch = useDispatch();
+  const [isOpen, setIsOpen] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const close = () => {
+    setIsOpen(false);
+  };
+
+  const open = () => {
+    setIsOpen(true);
+  }
+
+  const onConfirm = (id) => {
+    setIsLoading(true);
+    setTimeout(async () => {
+      try {
+        const { data } = userApi.delete(`/users/${id}`);
+        dispatch(getUsersThunks())
+      } catch (error) {
+        console.log(error);
+      }
+      setIsLoading(false);
+      setIsOpen(false);
+    }, 1000);
+  }
+
+  return {
+    close,
+    isLoading,
+    isOpen,
+    onConfirm,
+    open,
+  }
+}
