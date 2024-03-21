aaa63d96a15db9d6cda548515bf43dccdd729465
Abel Aron Amieva <abelamieva@gmail.com>
2024-03-20 22:23:50 -0300

    Refactorizacion

--- client/src/presentation/hooks/useModalDelUser.jsx
+++ client/src/presentation/hooks/useModalDelUser.jsx
@@ -5,37 +5,37 @@ import { getUsersThunks } from "../redux/features/users/usersThunks";
 
 export const useModalDelUser = () => {
 
-  const dispatch = useDispatch();
-  const [isOpen, setIsOpen] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
+	const dispatch = useDispatch();
+	const [isOpen, setIsOpen] = useState(false);
+	const [isLoading, setIsLoading] = useState(false);
 
-  const close = () => {
-    setIsOpen(false);
-  };
+	const close = () => {
+		setIsOpen(false);
+	};
 
-  const open = () => {
-    setIsOpen(true);
-  }
+	const open = () => {
+		setIsOpen(true);
+	}
 
-  const onConfirm = (id) => {
-    setIsLoading(true);
-    setTimeout(async () => {
-      try {
-        const { data } = userApi.delete(`/users/${id}`);
-        dispatch(getUsersThunks())
-      } catch (error) {
-        console.log(error);
-      }
-      setIsLoading(false);
-      setIsOpen(false);
-    }, 1000);
-  }
+	const onConfirm = (id) => {
+		setIsLoading(true);
+		setTimeout(async () => {
+			try {
+				const { data } = userApi.delete(`/users/${id}`);
+				dispatch(getUsersThunks())
+			} catch (error) {
+				console.log(error);
+			}
+			setIsLoading(false);
+			setIsOpen(false);
+		}, 1000);
+	}
 
-  return {
-    close,
-    isLoading,
-    isOpen,
-    onConfirm,
-    open,
-  }
+	return {
+		close,
+		isLoading,
+		isOpen,
+		onConfirm,
+		open,
+	}
 }
