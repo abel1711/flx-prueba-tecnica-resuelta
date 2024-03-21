4e39a0e8797467bbd32120ec50d649bcc49670ff
Abel Aron Amieva <abelamieva@gmail.com>
2024-03-21 00:09:16 -0300

    se corrigio error en la importacion

--- client/src/presentation/redux/store/appStore.js
+++ client/src/presentation/redux/store/appStore.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { usersSlice } from '@redux';
+import { usersSlice } from '../features/users/usersSlice';
 
 export const store = configureStore({
     reducer: {
